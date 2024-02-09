b553983ac54a3e9c5e860e79798a67d899a24503
lakindusathsara7 <158593383+lakindusathsara7@users.noreply.github.com>
2024-02-09 07:38:29 +0530

    Create calender.py
    Simple calender

---
+++ calender.py
@@ -0,0 +1,83 @@
+print('calendar')
+import calendar
+yy=2024#year
+mm=1#month
+#display the calendar
+print(calendar.month(yy,mm))
+
+print('calendar')
+import calendar
+yy=2024#year
+mm=2#month
+#display the calendar
+print(calendar.month(yy,mm))
+
+print('calendar')
+import calendar
+yy=2024#year
+mm=3#month
+#display the calendar
+print(calendar.month(yy,mm))
+
+print('calendar')
+import calendar
+yy=2024#year
+mm=4#month
+#display the calendar
+print(calendar.month(yy,mm))
+
+print('calendar')
+import calendar
+yy=2024#year
+mm=5#month
+#display the calendar
+print(calendar.month(yy,mm))
+
+print('calendar')
+import calendar
+yy=2024#year
+mm=6#month
+#display the calendar
+print(calendar.month(yy,mm))
+
+print('calendar')
+import calendar
+yy=2024#year
+mm=7#month
+#display the calendar
+print(calendar.month(yy,mm))
+
+print('calendar')
+import calendar
+yy=2024#year
+mm=8#month
+#display the calendar
+print(calendar.month(yy,mm))
+
+print('calendar')
+import calendar
+yy=2024#year
+mm=9#month
+#display the calendar
+print(calendar.month(yy,mm))
+
+print('calendar')
+import calendar
+yy=2024#year
+mm=10#month
+#display the calendar
+print(calendar.month(yy,mm))
+
+print('calendar')
+import calendar
+yy=2024#year
+mm=11#month
+#display the calendar
+print(calendar.month(yy,mm))
+
+print('calendar')
+import calendar
+yy=2024#year
+mm=12#month
+#display the calendar
+print(calendar.month(yy,mm))
